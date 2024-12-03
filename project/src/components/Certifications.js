8158824f3992775b4889c6c50960f66aca407ad7
Shalini <shalinikannan134@gmail.com>
2024-12-03 20:51:17 +0530

    Updated PORTFOLIO

--- project/src/components/Certifications.js
+++ project/src/components/Certifications.js
@@ -8,31 +8,31 @@ const Certifications = () => {
       <div className="certifications-container">
         <div className="certification-item">
           <h3>GENERATIVE AI</h3>
-          <a href={`${process.env.PUBLIC_URL}/certificates/GEN AI.pdf`} download>
+          <a href="/certificates/GEN AI.pdf" download>
             <button className="download-btn">Download</button>
           </a>
         </div>
         <div className="certification-item">
           <h3>ADVANCE JAVA PROGRAMMING</h3>
-          <a href={`${process.env.PUBLIC_URL}/certificates/NPTEL.pdf`} download>
+          <a href="/certificates/NPTEL.pdf" download>
             <button className="download-btn">Download</button>
           </a>
         </div>
         <div className="certification-item">
           <h3>OBJECT ORIENTED PROGRAMMING</h3>
-          <a href={`${process.env.PUBLIC_URL}/certificates/oops.pdf`} download>
+          <a href="/certificates/oops.pdf" download>
             <button className="download-btn">Download</button>
           </a>               
         </div>
         <div className="certification-item">
-          <h3>INTRODUCTION TO DEVOPS</h3>
-          <a href={`${process.env.PUBLIC_URL}/certificates/DEVOPS.pdf`} download>
+          <h3>INTRODUCTION TO DEVEOPS</h3>
+          <a href="/certificates/DEVOPS.pdf" download>
             <button className="download-btn">Download</button>
           </a>               
         </div>
         <div className="certification-item">
           <h3>USER INTERFACE AND USER EXPERIENCE</h3>
-          <a href={`${process.env.PUBLIC_URL}/certificates/UIUX.pdf`} download>
+          <a href="/certificates/UIUX.pdf" download>
             <button className="download-btn">Download</button>
           </a>               
         </div>
